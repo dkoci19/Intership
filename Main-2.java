dbd06b96e707afcb1a7ac3737687a12908400d34
Denian Koci <deniankoci@yahoo.com>
2025-11-17 14:43:28 +0100

    Dita e pare e Intership.

---
+++ Main-2.java
@@ -0,0 +1,84 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Ushtrimi.nr.1
+        //int age = 20;
+        //String name = "Denian";
+        //System.out.println("Une quhem "+ name +" dhe jam "+ age +" vjec.");
+
+        //Ushtrimi.nr.2
+        //int age = 20;
+        //double Pi = 3.14;
+        //boolean ne_shkolle = true;
+        //char Dollar = '$';
+
+        //System.out.println(age);
+        //System.out.println(Pi);
+        //System.out.println(ne_shkolle);
+        //System.out.println(Dollar);
+
+
+        //Ushtrimi.nr.3
+        //public class Main {
+        //  public static int Shuma (int a, int b){
+        //      return a+b;
+        //  }
+        //  public static void main(Strings []args){
+        //      int num1 = 4;
+        //      int num2 = 7;
+
+        //      int rezultati = Shuma(num1,num2);
+        //      System.out.println(rezultati);
+        //  }
+        //}
+
+        //Ushtrimi.nr.4
+        //public static double Numri(double a){
+        //  if(a < 0){
+        //      System.out.println("Numri eshte negativ.");
+        //   } else if (a > 0) {
+        //      System.out.println("Numri eshte pozitiv.");
+        //  } else {
+        //      System.out.println("Numri juaj eshte 0");
+        //}
+
+        //public static void main(String []args){
+        //  double num = 12.4;
+        //  Numri(num);
+        //}
+
+
+        //Ushtrimi.nr.5
+        //public static double Numri(double a){
+        //Perdora nje metode double.
+        //  if(a < 0){
+        //      System.out.println("Numri eshte negativ.");
+        //   } else if (a > 0) {
+        //      System.out.println("Numri eshte pozitiv.");
+        //  } else {
+        //      System.out.println("Numri juaj eshte 0");
+        //}
+
+        /*Me poshte bera nje conditiondal statment
+        if else per te pare nese numri ishte pozitiv ose negativ.
+         */
+        //public static void main(String []args){
+        //  double num = 12.4;
+        //  Numri(num);
+        //}
+
+
+        //Ushtrimi.nr.6
+        //final int age = 18;
+        //System.out.println("Mosha e lejuar per te votuar eshte 18 vjec.");
+
+        //Ushtrimi.nr.7
+        //int a = 5;Kjo eshte nje variabel e cila nuk mund te marr vlere boshe(null) aka unboxing.
+        //Integer a = 5; Kjo eshte nje klase mbjeshtellese(wrapper class) e cila pranon dhe null si vlere aka outboxing.
+        //Me duhej te kerkoja ne internet per kete sepse eshte hera e pare qe e shof si gje ne programim.
+    }
+}
