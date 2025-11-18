7d8be0d70d54ad2c23c96d0468b76bf56453834a
Denian Koci <deniankoci@yahoo.com>
2025-11-18 04:14:33 +0100

    Ushtrime me file,ushtrimi nr 1.

--- Main.java
+++ Main.java
@@ -1,83 +1,18 @@
-package org.example;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-class Building {
-    private long Id;
-    public String Addres;
+public static void writeToFile() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Shkruaj tekstin që do të ruhet në file: ");
+    String text = sc.nextLine();
 
-    public Building(long Id, String Addres) {
-        this.Id = Id;
-        this.Addres = Addres;
-    }
-
-    public long getId() {
-        return Id;
-    }
-    public void setId(long Id) {
-        this.Id = Id;
-    }
-
-    public String getAddres() {
-        return Addres;
-    }
-    public void setAddres(String Addres) {
-        this.Addres = Addres;
-    }
-
-    public void printAddress() {
-        System.out.println("Address: " + Addres);
-        System.out.println("ID: " + Id);
+    try {
+        FileWriter writer = new FileWriter("data.txt");
+        writer.write(text);
+        writer.close();
+        System.out.println("Teksti u ruajt me sukses në data.txt!");
+    } catch (IOException e) {
+        System.out.println("Gabim gjatë shkrimit në file.");
     }
 }
-
-class Residental extends Building {
-    int Apaertaments;
-
-    public Residental(long Id, String Addres, int Apaertaments) {
-        super(Id, Addres);
-        this.Apaertaments = Apaertaments;
-    }
-
-    public int getApaertaments() {
-        return Apaertaments;
-    }
-    public void setApaertaments(int Apaertaments) {
-        this.Apaertaments = Apaertaments;
-    }
-
-    public void printAddres() {
-        System.out.println("Address: " + Addres);
-    }
-}
-
-class Commercial extends Building {
-    private int officeNumber;
-    public Commercial(long Id, String Addres, int officeNumber) {
-        super(Id, Addres);
-        this.officeNumber = officeNumber;
-    }
-
-    public int getOfficeNumber() {
-        return  officeNumber;
-    }
-    public void setOfficeNumber(int officeNumber) {
-        this.officeNumber = officeNumber;
-    }
-
-    public void printAddress() {
-        System.out.println("Office number : " + officeNumber);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Building building = new Building(221, "Tirane");
-        building.printAddress();
-
-        Residental res = new Residental(222,"Durres",20);
-        res.printAddress();
-
-        Commercial commercial = new Commercial(223,"Tirane",20);
-        commercial.printAddress();
-    }
-}
-
