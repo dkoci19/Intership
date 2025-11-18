7b66d5dea4e6d3b222ac006f4f72c568b198a772
Denian Koci <deniankoci@yahoo.com>
2025-11-18 03:24:46 +0100

    Ushtrimi ekstra me klaxon Building

--- Main.java
+++ Main.java
@@ -28,9 +28,56 @@ class Building {
         System.out.println("ID: " + Id);
     }
 }
+
+class Residental extends Building {
+    int Apaertaments;
+
+    public Residental(long Id, String Addres, int Apaertaments) {
+        super(Id, Addres);
+        this.Apaertaments = Apaertaments;
+    }
+
+    public int getApaertaments() {
+        return Apaertaments;
+    }
+    public void setApaertaments(int Apaertaments) {
+        this.Apaertaments = Apaertaments;
+    }
+
+    public void printAddres() {
+        System.out.println("Address: " + Addres);
+    }
+}
+
+class Commercial extends Building {
+    private int officeNumber;
+    public Commercial(long Id, String Addres, int officeNumber) {
+        super(Id, Addres);
+        this.officeNumber = officeNumber;
+    }
+
+    public int getOfficeNumber() {
+        return  officeNumber;
+    }
+    public void setOfficeNumber(int officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public void printAddress() {
+        System.out.println("Office number : " + officeNumber);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Building building = new Building(221, "Tirane");
         building.printAddress();
+
+        Residental res = new Residental(222,"Durres",20);
+        res.printAddress();
+
+        Commercial commercial = new Commercial(223,"Tirane",20);
+        commercial.printAddress();
     }
 }
+
