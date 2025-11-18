1aaeab30e642baa734d8bfda8a3dbb3f6c6332b4
Denian Koci <deniankoci@yahoo.com>
2025-11-18 04:19:04 +0100

    Ushtrime me file,ushtrimim nr 3.

--- Main.java
+++ Main.java
@@ -1,18 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+public static int countLines(String fileName) {
+    int count = 0;
 
-public static void readFile() {
-    try {
-        BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+    try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        while (br.readLine() != null) {
+            count++;
         }
-
-        reader.close();
     } catch (IOException e) {
         System.out.println("Gabim gjatë leximit të file-it.");
     }
+
+    return count;
 }
